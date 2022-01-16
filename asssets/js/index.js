1c21c4143f5db31686b63b9714b1d137e3f8661d
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-01-16 14:12:17 +0000

    index.js

--- asssets/js/index.js
+++ asssets/js/index.js
@@ -1,130 +1,45 @@
-const timeBlockLabels = [
-  { label: "9 AM", key: 9 },
-  { label: "10 AM", key: 10 },
-  { label: "11 AM", key: 11 },
-  { label: "12 PM", key: 12 },
-  { label: "1 PM", key: 13 },
-  { label: "2 PM", key: 14 },
-  { label: "3 PM", key: 15 },
-  { label: "4 PM", key: 16 },
-  { label: "5 PM", key: 17 },
-];
+// Get current date for the header using moment.js
+const displayDate = moment();
+$("#currentDay").text(moment().format("dddd, MMMM Do HH:mm"));
 
-const currentDay = $("#current-day");
-const clockContainer = $("#clock");
+// Get current time
+function currentTime() {
+  const current = moment().hours();
+  const time = $(".time-block");
 
-const getDataFromLS = () => {
-  return JSON.parse(localStorage.getItem("textInput")) || {};
-};
-
-const onSave = (event) => {
-  const target = $(event.target);
-
-  if (
-    target.is('button[name="save-btn"]') ||
-    target.is('i[name="save-icon"]')
-  ) {
-    const data = getDataFromLS();
-    const key = target.attr("id");
-
-    data[key] = $(`textarea[id="${key}"]`).val();
-
-    localStorage.setItem("textInput", JSON.stringify(data));
-  }
-};
-
-const getTextAreaValue = (key) => {
-
-  const data = getDataFromLS();
-
-  return data[key];
-};
-
-const getClassName = (key) => {
-  const currentTime = moment().hour();
-
-  const isPast = currentTime > key;
-  const isFuture = currentTime < key;
-
-  // check if it is past, future or present
-  if (isPast) {
-    return "textarea-container";
-  } else if (isFuture) {
-    return "textarea-future";
-  } else {
-    return "textarea-present";
-  }
-};
+  time.each(function () {
+    // parse string and return integer
+    const hour = parseInt($(this).attr("id"));
 
 
 
-const renderTimeBlocks = () => {
-  const constructTimeBlocks = (each) => {
-    //render timeBlock
-    const timeBlock = `<div class="card-container" id="card-container">
-      <div class="hour-container">
-        <h2 class="hour">${each.label}</h2>
-      </div>
-      <div class="${getClassName(each.key)}">
-        <textarea class="textarea" id=${each.key} data="${each.key}">${
-      getTextAreaValue(each.key) || ""
-    }</textarea>
-      </div>
-      <div id="btn-container" class="btn-container">
-        <button name="save-btn" id=${
-          each.key
-        } class="save-btn"><i name="save-icon" id=${
-      each.key
-    } class="far fa-save"></i></i></button>
-      </div>
-    </div>`;
+    // if hour === current time display present colours.
+    if (hour === current) {
+      $(this).children(".col-10").attr("class", "present col-10 schedule");
 
-    return timeBlock;
-  };
+      // Else if hour > current time display present colours.
+    } else if (current > hour) {
+      $(this).children(".col-10").attr("class", "past col-10 schedule");
 
-  //map over the timeBlockLabels to construct each TimeBlock
-  const timeBlocks = timeBlockLabels.map(constructTimeBlocks).join("");
+      /// if hour < current time, display future colours.
+    } else {
+      $(this).children(".col-10").attr("class", "future col-10 schedule");
+    }
+  });
+}
 
-  //append to main
-  $("#time-block-container").append(timeBlocks);
-};
+// return current time
+currentTime();
 
-const renderCurrentDay = () => {
-  currentDay.text(moment().format("dddd MMMM Do YYYY"));
-};
+// Target save button
+const button = $(".saveBtn");
 
-const setClock = () => {
-  clockContainer.text(moment().format("kk:mm:ss"));
-};
+/
 
-const renderClock = () => {
-  setInterval(setClock, 1000);
-};
+button.on("click", function () {
+  const time = $(this).parent().attr("id");
+  const schedule = $(this).siblings(".schedule").val();
 
-const onLoad = () => {
-  renderCurrentDay();
-
-  setClock();
-
-  renderClock();
-
-  renderTimeBlocks();
-};
-
-
-
-const onClear = () => {
-  localStorage.clear();
-
-  $("#time-block-container").empty();
-
-  renderTimeBlocks();
-};
-
-// add a event listener click to save button
-$("#time-block-container").on("click", onSave);
-
-// add a event listener click to clear all button
-$("#clear-all-btn").on("click", onClear);
-
-$(document).ready(onLoad);
+  // set user input to local storage
+  localStorage.setItem(time, schedule);
+});
